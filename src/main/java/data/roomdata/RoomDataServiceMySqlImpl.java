34467006ef33d3b370b657280b3de269065e1441
zhuangyz1998 <zhuangyz1998@icloud.com>
2017-01-02 01:05:33 +0800

    room

--- src/main/java/data/roomdata/RoomDataServiceMySqlImpl.java
+++ src/main/java/data/roomdata/RoomDataServiceMySqlImpl.java
@@ -12,18 +12,18 @@ import dataservice.roomdataservice.RoomDataService;
 import po.RoomPO;
 import po.RoomType;
 
-public class RoomDataServiceMySqlImpl implements RoomDataService{
+public class RoomDataServiceMySqlImpl implements RoomDataService {
 
 	Connection connect;
 	PreparedStatement statement;
 	ResultSet result;
 	ConfigurationServiceMySqlImpl configure;
-	
+
 	public RoomDataServiceMySqlImpl() {
 		// TODO Auto-generated constructor stub
 		configure = new ConfigurationServiceMySqlImpl();
 	}
-	
+
 	/**
 	 * 
 	 * @param rpo
@@ -32,18 +32,19 @@ public class RoomDataServiceMySqlImpl implements RoomDataService{
 	public void insertRoom(RoomPO rpo) throws RemoteException {
 		// TODO Auto-generated method stub
 		try {
-			//为防止重复插入信息应进行检查
-			//if(findRoom(rpo.getHotelID(), rpo.getRoomType()) != null) return;    //不知为何加上会报错
-			
-			//列：id; roomtype; total; remain; price
+			// 为防止重复插入信息应进行检查
+			// if(findRoom(rpo.getHotelID(), rpo.getRoomType()) != null) return;
+			// //不知为何加上会报错
+
+			// 列：id; roomtype; total; remain; price
 			statement = connect.prepareStatement("insert into room values(?, ?, ?, ?, ?)");
-			
+
 			statement.setString(1, String.valueOf(rpo.getHotelID()));
 			statement.setString(2, String.valueOf(rpo.getRoomType()));
 			statement.setString(3, String.valueOf(rpo.getTotalSum()));
 			statement.setString(4, String.valueOf(rpo.getRemainSum()));
 			statement.setString(5, String.valueOf(rpo.getPrice()));
-			
+
 			statement.executeUpdate();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -59,10 +60,10 @@ public class RoomDataServiceMySqlImpl implements RoomDataService{
 	public void deleteAllRooms(int id) throws RemoteException {
 		// TODO Auto-generated method stub
 		try {
-			//列：id; roomtype; total; remain; price
+			// 列：id; roomtype; total; remain; price
 			statement = connect.prepareStatement("delete from room where id = ?");
 			statement.setString(1, String.valueOf(id));
-			
+
 			statement.execute();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -79,11 +80,11 @@ public class RoomDataServiceMySqlImpl implements RoomDataService{
 	public void deleteRoom(int id, RoomType type) throws RemoteException {
 		// TODO Auto-generated method stub
 		try {
-			//列：id; roomtype; total; remain; price
+			// 列：id; roomtype; total; remain; price
 			statement = connect.prepareStatement("delete from room where id = ? and roomtype = ?");
 			statement.setString(1, String.valueOf(id));
 			statement.setString(2, String.valueOf(type));
-			
+
 			statement.execute();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -101,12 +102,12 @@ public class RoomDataServiceMySqlImpl implements RoomDataService{
 	public void updatePrice(int id, RoomType type, int price) throws RemoteException {
 		// TODO Auto-generated method stub
 		try {
-			//列：id; roomtype; total; remain; price
+			// 列：id; roomtype; total; remain; price
 			statement = connect.prepareStatement("update room set price = ? where id = ? and roomtype = ?");
 			statement.setString(1, String.valueOf(price));
 			statement.setString(2, String.valueOf(id));
 			statement.setString(3, String.valueOf(type));
-			
+
 			statement.executeUpdate();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -124,12 +125,12 @@ public class RoomDataServiceMySqlImpl implements RoomDataService{
 	public void updateRemainSum(int id, RoomType type, int remain) throws RemoteException {
 		// TODO Auto-generated method stub
 		try {
-			//列：id; roomtype; total; remain; price
+			// 列：id; roomtype; total; remain; price
 			statement = connect.prepareStatement("update room set remain = ? where id = ? and roomtype = ?");
 			statement.setString(1, String.valueOf(remain));
 			statement.setString(2, String.valueOf(id));
 			statement.setString(3, String.valueOf(type));
-			
+
 			statement.executeUpdate();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -147,12 +148,12 @@ public class RoomDataServiceMySqlImpl implements RoomDataService{
 	public void updateTotalSum(int id, RoomType type, int total) throws RemoteException {
 		// TODO Auto-generated method stub
 		try {
-			//列：id; roomtype; total; remain; price
+			// 列：id; roomtype; total; remain; price
 			statement = connect.prepareStatement("update room set total = ? where id = ? and roomtype = ?");
 			statement.setString(1, String.valueOf(total));
 			statement.setString(2, String.valueOf(id));
 			statement.setString(3, String.valueOf(type));
-			
+
 			statement.executeUpdate();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -171,28 +172,30 @@ public class RoomDataServiceMySqlImpl implements RoomDataService{
 		// TODO Auto-generated method stub
 		try {
 			statement = connect.prepareStatement("select * from room where id = ?");
-			
-			//列：id; roomtype; total; remain; price
+
+			// 列：id; roomtype; total; remain; price
 			statement.setString(1, String.valueOf(id));
-			
+
 			result = statement.executeQuery();
-			
-			//RoomPO: id, roomtype, total, remain, price
+
+			// RoomPO: id, roomtype, total, remain, price
 			int total = -1;
 			int remain = -1;
 			int price = -1;
-			
-			//遍历result
-			while(result.next()){
-				total = Integer.valueOf(result.getString(3));
-				remain = Integer.valueOf(result.getString(4));
-				price = Integer.valueOf(result.getString(5));
+
+			// 遍历result
+			while (result.next()) {
+				if (result.getString(2).equals(type.toString())) {
+					total = Integer.valueOf(result.getString(3));
+					remain = Integer.valueOf(result.getString(4));
+					price = Integer.valueOf(result.getString(5));
+				}
 			}
-			
-			if(total != -1){
+
+			if (total != -1) {
 				return new RoomPO(id, type, total, remain, price);
 			}
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -207,31 +210,31 @@ public class RoomDataServiceMySqlImpl implements RoomDataService{
 	public ArrayList<RoomPO> findRooms(int id) throws RemoteException {
 		// TODO Auto-generated method stub
 		try {
-			///列：id; roomtype; total; remain; price
-			
+			/// 列：id; roomtype; total; remain; price
+
 			statement = connect.prepareStatement("select * from room where id = ?");
 			statement.setString(1, String.valueOf(id));
-			result = statement.executeQuery(); 
-			
+			result = statement.executeQuery();
+
 			RoomType roomtype = null;
 			int totalSum = -1;
 			int remainSum = -1;
 			int price = -1;
-			
+
 			ArrayList<RoomPO> rpoList = new ArrayList<RoomPO>();
-			
-			while(result.next()){
-				//id = Integer.valueOf(result.getString(1));
+
+			while (result.next()) {
+				// id = Integer.valueOf(result.getString(1));
 				roomtype = RoomType.valueOf(result.getString(2));
-				totalSum =  Integer.valueOf(result.getString(3));
-				remainSum =  Integer.valueOf(result.getString(4));
-				price =  Integer.valueOf(result.getString(5));
-				
+				totalSum = Integer.valueOf(result.getString(3));
+				remainSum = Integer.valueOf(result.getString(4));
+				price = Integer.valueOf(result.getString(5));
+
 				RoomPO rpo = new RoomPO(id, roomtype, totalSum, remainSum, price);
 				rpoList.add(rpo);
 			}
-			
-			if(id != -1){
+
+			if (id != -1) {
 				return rpoList;
 			}
 		} catch (SQLException e) {
@@ -240,7 +243,7 @@ public class RoomDataServiceMySqlImpl implements RoomDataService{
 		}
 		return null;
 	}
-	
+
 	/**
 	 * 
 	 * @throws RemoteException
